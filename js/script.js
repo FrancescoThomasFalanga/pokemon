4b731be71dd664c5f4ba450fa1cdca9afd951c63
FrancescoThomasFalanga <francescoasdf3124@gmail.com>
2023-11-14 14:46:45 +0100

    add reset bonus button

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 let punteggioAttuale = parseInt(localStorage.getItem('punteggio')) || 0;
 
+let resetCounter = parseInt(localStorage.getItem('resetCounter')) || 0;
+
 aggiornaPunteggio();
+aggiornaCounter();
 
 // Array di bonus dal "database"
-const bonusDatabase = [
+const bonusDatabase = JSON.parse(localStorage.getItem('bonusDatabase')) || [
     { title: "Out Of Lab", points: 20 },
     { title: "Torre Sprout", points: 25 },
     { title: "Brock", points: 70 },
@@ -49,6 +52,11 @@ bonusDatabase.forEach(bonus => {
 // Carica bonus salvati nel localStorage
 caricaBonusSalvati();
 
+function rimuoviBonusDallaLista(selectedBonusIndex) {
+    bonusDatabase.splice(selectedBonusIndex, 1);
+    bonusSelect.remove(selectedBonusIndex);
+}
+
 function aggiungiBonusInPagina() {
     // Recupera il bonus selezionato
     const selectedBonusIndex = bonusSelect.selectedIndex;
@@ -59,11 +67,76 @@ function aggiungiBonusInPagina() {
         // Visualizza il bonus in pagina
         visualizzaBonusInPagina(selectedBonus);
 
+        // Rimuovi il bonus dalla lista dei bonus disponibili
+        rimuoviBonusDallaLista(selectedBonusIndex);
+
         // Aggiungi il bonus al localStorage
         salvaBonus(selectedBonus);
     } else {
         alert("Questo bonus è già presente in pagina.");
     }
+}
+
+// Funzione per ripristinare la lista dei bonus
+function resetBonus() {
+
+    // Incrementa il contatore al click
+    resetCounter++;
+
+    // Aggiorna il valore del contatore nell'elemento HTML
+    document.getElementById('resetCounter').textContent = `C = ${resetCounter}`;
+
+    // Aggiorna il valore del contatore nel local storage
+    localStorage.setItem('resetCounter', resetCounter);
+
+    // Ripristina la lista dei bonus dal "database"
+    bonusSelect.innerHTML = ""; // Rimuovi tutte le opzioni nel select
+
+    let copyBonusDatabase = [
+        { title: "Out Of Lab", points: 20 },
+        { title: "Torre Sprout", points: 25 },
+        { title: "Brock", points: 70 },
+        { title: "Union Cave", points: 10 },
+        { title: "Bugsy", points: 25 },
+        { title: "Rivale Post Bugsy", points: 25 },
+        { title: "Whitney", points: 15 },
+        { title: "Torre Bruciata", points: 10 },
+        { title: "Rivale Torre", points: 10 },
+        { title: "Mt. Mortar", points: 10 },
+        { title: "Faro", points: 10 },
+        { title: "Nina", points: 15 },
+        { title: "Grotta Celeste", points: 10 },
+        { title: "Team Rocket HQ", points: 20 },
+        { title: "Erika", points: 20 },
+        { title: "Jasmine", points: 35 },
+        { title: "Pryce", points: 20 },
+        { title: "T. Radio #1", points: 10 },
+        { title: "Tunnel e T. Radio #2", points: 25 },
+        { title: "Rivale Tunnel", points: 25 },
+        { title: "Clair", points: 40 },
+        { title: "Kimono", points: 20 },
+        { title: "Rivale Via Vittoria", points: 40 },
+        { title: "Via Vittoria", points: 20 },
+        { title: "Will", points: 100 },
+        { title: "Koga", points: 130 },
+        { title: "Bruno", points: 160 },
+        { title: "Karen", points: 200 },
+        { title: "Lance", points: 500 },
+        { title: "Blu", points: 350 },
+        { title: "Rosso", points: 700 },
+    ]
+
+    // Popola dinamicamente le opzioni del select con la lista completa di bonus
+    copyBonusDatabase.forEach(bonus => {
+        const option = document.createElement("option");
+        option.text = bonus.title;
+        bonusSelect.add(option);
+    });
+
+    // Salva la lista completa dei bonus nel localStorage
+    localStorage.setItem('bonusDatabase', JSON.stringify(copyBonusDatabase));
+
+    aggiornaCounter()
 }
 
 function isBonusPresente(bonus) {
@@ -152,9 +225,20 @@ function aggiornaPunteggio() {
     punteggioElement.textContent = punteggioAttuale;
 }
 
+function aggiornaCounter() {
+    const counterElement = document.getElementById("resetCounter");
+    counterElement.textContent = "Tentativo N." + resetCounter;
+}
+
 function resetPunteggio() {
     localStorage.removeItem('punteggio');
     document.getElementById('punteggio').innerText = '0';
+    localStorage.removeItem("bonusSalvati");
+    localStorage.removeItem("bonusDatabase");
+    localStorage.removeItem("resetCounter");
+
+    location.reload();
+
 }
 
 function aggiungiPuntiEvo() {
